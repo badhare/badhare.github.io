cc18e64f8196f30f04dcbb905abdd7a2dd2c1f8a
ChenJiang <chenjiang1@superatomfin.com>
2019-08-07 15:06:13 +0800

    first commit

---
+++ script.js
@@ -0,0 +1,25 @@
+new Vue({
+    el: '#app',
+    data: {
+        day: 888,
+        hour: 888,
+        minute: 888,
+        second: 8888,
+        start: "2011-11-11 12:00:00",
+        startMoment: null,
+    },
+    mounted: function(){
+        this.startMoment = moment(this.start,"YYYY-MM-DD HH:mm:ss").utc().zone(+8);
+        setInterval(this.timer, 1000);
+    },
+    methods: {
+        timer: function () {
+            var now = moment().utc().zone(+8);
+            this.day = now.diff(this.startMoment,"days");
+            this.hour = now.diff(this.startMoment,"hours") - this.day * 24;
+            this.minute = now.diff(this.startMoment,"minutes") - (this.day * 24 + this.hour) * 60;
+            this.second = now.diff(this.startMoment,"seconds") - this.day * 86400 - this.hour * 3600 - this.minute * 60;
+            console.log(this.day,this.hour,this.minute,this.second)
+        }
+    }
+});
